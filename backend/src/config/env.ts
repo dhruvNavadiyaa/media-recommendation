bd3fbbdd8dae4bdbd89ea5fd0ad1c02e43c16179
Yash Talaviya <yashtalaviya654@gmail.com>
2025-09-14 21:34:49 +0530

    Add backend dependencies and set up Express server with middleware

---
+++ backend/src/config/env.ts
@@ -0,0 +1,9 @@
+const MONGO_URI = process.env.MONGO_URI as string;
+
+const PORT = process.env.PORT as unknown as number;
+
+
+export {
+  MONGO_URI,
+  PORT,
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello World");
+import "./config/envConfig";
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import { PORT } from "./config/env";
+import db from "./config/db";
+
+
+
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+
+db();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms ")
+);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Running on ${PORT}`);
+});
